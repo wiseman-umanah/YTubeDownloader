b517058bbfe1499983c9e952b6cd7532509153a2
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-18 23:24:49 +0100

    Added function for return/enter key handling

--- VidGUI.py
+++ VidGUI.py
@@ -11,13 +11,19 @@ def get_input(event):
 	user_input = entry.get()
 	print(user_input)
 
+
+def create_button():
+	download_button = tk.Button(root, text="Download", command=lambda: get_input("<Return>"), foreground="white", background="red")
+	download_button.pack(side=tk.RIGHT, padx=(0, 50), pady=(5, 0))
+
+
 write = ttk.Label(root, text="Paste your youtube Link to Start Downloading", font=("Helvetica", 15), foreground="#066691", padding=10)
 write.pack()
-entry = ttk.Entry(root, width=80)
+
+entry_var = tk.StringVar()
+entry_var.trace_add("write", create_button)
+entry = ttk.Entry(root, width=80, textvariable=entry_var)
 entry.pack()
 entry.bind("<Return>", get_input)
-download_button = tk.Button(root, text="Download", command=lambda: get_input("<Return>"), foreground="white", background="red")
-download_button.pack(side=tk.RIGHT, padx=(0, 50), pady=(5, 0))
-
 
 root.mainloop()
