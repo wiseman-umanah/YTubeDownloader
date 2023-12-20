45bb18fdced4e1f0b67eaf58ce129eeebf32f034
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-21 00:21:25 +0100

    Added function to handle image files in program

--- VidGUI.py
+++ VidGUI.py
@@ -7,6 +7,17 @@ from main import VidDownloader
 import os
 import sys
 
+# To make image files in code linked to the program
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 
 #Initialization of GUI module and window formatting
 
