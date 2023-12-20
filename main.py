d140c2b41beaa9c9590416bb75a69c7b3e1eb27b
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-20 03:17:43 +0100

    Added docstring to main module

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+"""
+This Module is the backbone of the youtube video/audio downloader
+"""
 from pytube import YouTube
 import re
 import os
 
+
 class VidDownloader(YouTube):
+	"""
+	This class inherits from pytube to download youtube Video
+
+	Parameters:
+		link (any): The link passed to the class
+
+	Attributes:
+		download_audVid: This function downloads the video/audio from youtube
+	
+	Raises:
+		TypeError: If argument is empty or None
+		TypeError: If link fails to intialize
+	"""
 	def __init__(self, link=None):
 		self.download_path = str(os.path.expanduser("~\Downloads"))
 		if link == None or "":
@@ -16,6 +33,15 @@ class VidDownloader(YouTube):
 
 	@property
 	def link(self):
+		"""Sets/Get the value of link
+		
+		Parameters:
+			value: the new link that is set
+		
+		Raises:
+			TypeError: If a wrong Url is passed
+			
+		"""
 		return self.__link
 	
 	@link.setter
@@ -27,6 +53,13 @@ class VidDownloader(YouTube):
 			raise TypeError("Wrong url passed")
 		
 	def download_audVid(self, quality="high", type="Video"):
+		"""The function to download the audio/video from youtube
+		
+		Parameters:
+			quality (str): The quality option of the youtube video
+			
+			type (str): The type of media whether Video or Audio
+		"""
 		if type == "Audio":
 			if quality == "low":
 				self.ylink.streams.filter(mime_type="audio/mp4", type="audio").order_by("bitrate").desc().last().download(self.download_path)
