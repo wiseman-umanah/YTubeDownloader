219089a41cb62d4d8ced26f70920890ce4905812
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-21 03:30:48 +0100

    Test files added and waiting to be worked upon

---
+++ tests/testMain.py
@@ -0,0 +1,7 @@
+import main
+import unittest
+
+class MajorTest(unittest.TestCase):
+	pass
+if __name__ == "__main__":
+	pass
---
+++ tests/testVidGUI.py
@@ -0,0 +1,10 @@
+import VidGUI
+import os
+import unittest
+
+
+class testGUI(unittest.TestCase):
+	pass
+
+if __name__ == "__main__":
+	pass
