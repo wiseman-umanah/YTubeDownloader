5dd2cb887f1cb0ccedb96192c1773add06715718
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-28 22:41:10 +0100

    Changed the program UI to look Modern

--- VidGUI.py
+++ VidGUI.py
@@ -1,18 +1,19 @@
 """
 This module handles all the GUI formats for end-user
 """
-import tkinter as tk
-from tkinter import ttk
+import customtkinter
 from main import VidDownloader
 
 
 #Initialization of GUI module and window formatting
 
-root = tk.Tk()
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+root = customtkinter.CTk()
 root.title("Youtube Downloader")
-root.geometry(newGeometry="430x180")
+root.geometry("380x180+400+200")
 root.resizable(False, False)
-root.configure(bg="black")
 
 #Handle user input, somewhat like a link to the backend of the program
 #Handle responds and error messages
@@ -36,77 +37,77 @@ def get_input(event):
 		test = VidDownloader(user_input)
 
 		# Create a new label widget
-		get_input.write1 = ttk.Label(root, text=" ", font=("Helvetica", 10), background="black")
-		get_input.write1.grid(row=2, column=0, padx=(5, 0), pady=(20, 10), sticky="w")
 		test.download_audVid(quality_option.get(), download_option.get())
-		get_input.write1.config(text=good, font=("Helvetica", 10), foreground="green",  background="black")
-		get_input.write1.grid(row=2, column=0, padx=(5, 0), pady=(20, 10), sticky="w")
+		get_input.write1= customtkinter.CTkLabel(root, text=good, font=("Helvetica", 12), text_color="green")
+		get_input.write1.grid(row=2, column=0, padx=(5, 0), sticky="w")
 	except Exception as e:
 		# Handle exceptions and display an error message
-		get_input.write1 = ttk.Label(root, text=bad, foreground="red", background="black")
-		get_input.write1.grid(row=2, column=0, padx=(5, 0), pady=(20, 10), sticky="w")
+		get_input.write1 = customtkinter.CTkLabel(root, text=bad, font=("Helvetica", 12), text_color="red")
+		get_input.write1.grid(row=2, column=0, padx=(5, 0), sticky="w")
 
 
 ## Gets download option and sets default option
-download_option = tk.StringVar()
+download_option = customtkinter.StringVar()
 download_option.set("Video")
 
 
 ## Gets download quality option and sets default option
-quality_option = tk.StringVar()
+quality_option = customtkinter.StringVar()
 quality_option.set("high")
 
 
 ## Instruction to user
-Ins = "Paste your YouTube Link to Start Downloading"
-write = ttk.Label(root, text=Ins, font=("Helvetica", 15), foreground="#066691", padding=10,  background="black")
+Ins = "The YouTube Video Downloader"
+write = customtkinter.CTkLabel(root, text=Ins, font=("Helvetica", 15), text_color="#00eeff", padx=10)
 write.grid(row=0, column=0)
 
 
 ## Input bar for user's input
-entry = ttk.Entry(root, width=60)
-entry.grid(row=1, column=0)
+entry = customtkinter.CTkEntry(root, width=350)
+entry.configure(corner_radius=5, placeholder_text="Paste the youtube video URL", placeholder_text_color="grey")
+entry.grid(row=1, column=0, padx=15)
 entry.bind("<Return>", get_input)
 
 
 ## Format for Download Button
-download_button = tk.Button(root, text="Download", command=lambda: get_input("<Return>"), foreground="white", background="red")
-download_button.grid(row=2, column=0, sticky="e", pady=5, padx=(0, 30))
-download_button.configure(borderwidth=5, relief="flat")
+download_button = customtkinter.CTkButton(root, text="Download", command=lambda: get_input("<Return>"))
+download_button.configure(text_color="white", fg_color="red", corner_radius=5, hover=True, width=80, hover_color="#ff5057")
+download_button.grid(row=2, column=0, sticky="e", pady=5, padx=(0, 15))
 
 
 ## Format for download options
 vidText = "Please select file type:\t"
-video_frame = tk.Frame(root)
-video_frame.grid(row=3, column=0, sticky="e", padx=(0, 100))
-video_option = tk.Radiobutton(video_frame, text="Video", variable=download_option, value="Video", foreground="white", background="black")
-audio_option = tk.Radiobutton(video_frame, text="Audio", variable=download_option, value="Audio", foreground="white", background="black")
-vidType = tk.Label(root, text=vidText, foreground="white", background="black")
+video_frame = customtkinter.CTkFrame(root, fg_color="transparent")
+video_frame.grid(row=3, column=0, sticky="e")
+video_option = customtkinter.CTkRadioButton(video_frame, text="Video", variable=download_option, value="Video", text_color="white")
+audio_option = customtkinter.CTkRadioButton(video_frame, text="Audio", variable=download_option, value="Audio", text_color="white")
+vidType = customtkinter.CTkLabel(root, text=vidText, text_color="white")
 
 ## Positioning download options
 ## Styling
 vidType.grid(row=3, column=0, sticky="w", padx=(5, 0))
+video_option.configure(radiobutton_width=10, radiobutton_height=10, fg_color="#00eeff")
+audio_option.configure(radiobutton_width=10, radiobutton_height=10, fg_color="#00eeff")
 video_option.grid(row=0, column=0)
 audio_option.grid(row=0, column=1)
-video_option.configure(selectcolor="black")
-audio_option.configure(selectcolor="black")
 
 
 ## Format for download quality options
-qualText = "Please select file quality: "
-quality_frame = tk.Frame(root)
-quality_frame.grid(row=4, column=0, sticky="e", padx=(0, 114))
-quality1 = tk.Radiobutton(quality_frame, text="High", variable=quality_option, value="high", foreground="white", background="black")
-quality2 = tk.Radiobutton(quality_frame, text="Low", variable=quality_option, value="low", foreground="white", background="black")
-vidQual = tk.Label(root, text=qualText, foreground="white", background="black")
+qualText = "Please select file quality:\t"
+quality_frame = customtkinter.CTkFrame(root, fg_color="transparent")
+quality_frame.grid(row=4, column=0, sticky="e")
+quality1 = customtkinter.CTkRadioButton(quality_frame, text="High", variable=quality_option, value="high", text_color="white")
+quality2 = customtkinter.CTkRadioButton(quality_frame, text="Low", variable=quality_option, value="low", text_color="white")
+vidQual = customtkinter.CTkLabel(root, text=qualText, text_color="white")
 
 ## Positioning download quality options
 ## Styling
 vidQual.grid(row=4, column=0, sticky="w", padx=(5, 0))
+quality1.configure(radiobutton_width=10, radiobutton_height=10, fg_color="#00eeff")
+quality2.configure(radiobutton_width=10, radiobutton_height=10, fg_color="#00eeff")
 quality1.grid(row=0, column=0)
 quality2.grid(row=0, column=1)
-quality1.configure(selectcolor="black")
-quality2.configure(selectcolor="black")
+
 
 
 root.mainloop()
